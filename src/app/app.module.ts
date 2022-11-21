ac3f97c179eecf195cf6a6f322fff2ca5ed14984
Oluwatayoh <ojotayoh2@gmail.com>
2022-11-21 18:08:25 +0100

    test done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { HeroSectionComponent } from './sections/hero-section/hero-section.component';
+import { GallerySectionComponent } from './sections/gallery-section/gallery-section.component';
+import { ContactUsComponent } from './sections/contact-us/contact-us.component';
+import { FooterSectionComponent } from './sections/footer-section/footer-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    HeroSectionComponent,
+    GallerySectionComponent,
+    ContactUsComponent,
+    FooterSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
